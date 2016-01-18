7688aa661da742ba0fd21329e1a19f4243f7f630
charlocharlie <charlocharlie@yahoo.com>
2016-01-18 16:25:08 -0600

    Added comma to nodecg.json's, added multi-game support to crew-roster

--- ssbm-crew-roster/view/crew-roster.js
+++ ssbm-crew-roster/view/crew-roster.js
@@ -1,8 +1,11 @@
 'use strict';
-
+var halfWidth = 436;
+var leftEdge = 161;
+var border = 8;
 $(function() {
 	nodecg.listenFor('ssbmCrewUpdate', updateRosters);
 	nodecg.listenFor('ssbmCrewHide', hideRosters);
+    nodecg.listenFor('smashLayoutUpdate', 'smash-game-switcher', updateLayout);
 
 	function updateRosters(data) {
 		var rosterSize = 10;
@@ -48,19 +51,19 @@ $(function() {
 	function showRosters() {
 		var delay = 50;
 		var rosterSize = 10;
-		$('#rosterheaderleft').animate({left: "0%"}, 500);
-		$('#rosterheaderright').animate({left: "0%"}, 500);
+		$('#rosterheaderleft').animate({left: '0%'}, 500);
+		$('#rosterheaderright').animate({left: '0%'}, 500);
 		for (var i = 1; i <= rosterSize; i++) {
 			$('#rosterleft > div:nth-child(' + i + ')')
 				.delay(delay * i)
 				.queue(function(nxt) { 
-					$(this).animate({left: "0%"}, 500);
+					$(this).animate({left: '0%'}, 500);
 					nxt();
 				});
 			$('#rosterright > div:nth-child(' + i + ')')
 				.delay(delay * i)
 				.queue(function(nxt) { 
-					$(this).animate({left: "0%"}, 500);
+					$(this).animate({left: '0%'}, 500);
 					nxt();
 				});
 		}
@@ -71,23 +74,74 @@ $(function() {
 	function hideRosters(data) {
 		var delay = 50;
 		var rosterSize = 10;
-		$('#rosterheaderleft').animate({left: "-436px"}, 500);
-		$('#rosterheaderright').animate({left: "436px"}, 500);
+		$('#rosterheaderleft').animate({left: -leftEdge - 275 - 8 + border + "px"}, 500);
+		$('#rosterheaderright').animate({left: halfWidth + 8 + "px"}, 500);
 		for (var i = 1; i <= rosterSize; i++) {
 			$('#rosterleft > div:nth-child(' + i + ')')
 				.delay(delay * i)
 				.queue(function(nxt) { 
-					$(this).animate({left: "-436px"}, 500);
+					$(this).animate({left: -leftEdge - 275 - 8 + border + "px"}, 500);
 					nxt();
 				});
 			$('#rosterright > div:nth-child(' + i + ')')
 				.delay(delay * i)
 				.queue(function(nxt) { 
-					$(this).animate({left: "436px"}, 500);
+					$(this).animate({left: halfWidth + 8 + "px"}, 500);
 					nxt();
 				});
 		}
 		//$('#rosterleft').animate({left: "-275px"}, 500);
 		//$('#rosterright').animate({left: "275px"}, 500);
 	}
-})
+    function updateLayout(data) {
+        if (data.game == '64') { //SMASH 64
+            halfWidth = 472;
+            border = 8;
+            leftEdge = 205;
+            $('#rosterheadercontainer').css({left: leftEdge + "px"});
+            $('#rosterheaderleft').css({left: -leftEdge - 275 - 8 + border + "px"});
+            $('#rosterheaderright').css({left: halfWidth + 8 + "px"});
+            $('#rosterleft').css({left: leftEdge + "px"});
+            $('#rosterright').css({left: leftEdge + "px"});
+            $('#rosterleft .rosterentry').css({left: -leftEdge - 275 - 8 + border + "px"});
+            $('#rosterright .rosterentry').css({left: halfWidth + 8 + "px"});
+        }
+        if (data.game == 'melee') { //MELEE
+            halfWidth = 448;
+            border = 8;
+            leftEdge = 161;
+            $('#rosterheadercontainer').css({left: leftEdge + "px"});
+            $('#rosterheaderleft').css({left: -leftEdge - 275 - 8 + border + "px"});
+            $('#rosterheaderright').css({left: halfWidth + 8 + "px"});
+            $('#rosterleft').css({left: leftEdge + "px"});
+            $('#rosterright').css({left: leftEdge + "px"});
+            $('#rosterleft .rosterentry').css({left: -leftEdge - 275 - 8 + border + "px"});
+            $('#rosterright .rosterentry').css({left: halfWidth + 8 + "px"});
+        }
+        if (data.game == 'pm') { //PROJECT M/BRAWL
+            halfWidth = 456;
+            border = 0;
+            leftEdge = 181;
+            $('#rosterheadercontainer').css({left: leftEdge + "px"});
+            $('#rosterheaderleft').css({left: -leftEdge - 275 - 8 + border + "px"});
+            $('#rosterheaderright').css({left: halfWidth + 8 + "px"});
+            $('#rosterleft').css({left: leftEdge + "px"});
+            $('#rosterright').css({left: leftEdge + "px"});
+            $('#rosterleft .rosterentry').css({left: -leftEdge - 275 - 8 + border + "px"});
+            $('#rosterright .rosterentry').css({left: halfWidth + 8 + "px"});
+        }
+        if (data.game == 'wiiu') { //SMASH FOR WII U
+            halfWidth = 640;
+            border = 0;
+            leftEdge = 365;
+            $('#rosterheadercontainer').css({left: leftEdge + "px"});
+            $('#rosterheaderleft').css({left: -leftEdge - 275 - 8 + border + "px"});
+            $('#rosterheaderright').css({left: halfWidth + 8 + "px"});
+            $('#rosterleft').css({left: leftEdge + "px"});
+            $('#rosterright').css({left: leftEdge + "px"});
+            $('#rosterleft .rosterentry').css({left: -leftEdge - 275 - 8 + border + "px"});
+            $('#rosterright .rosterentry').css({left: halfWidth + 8 + "px"});
+        }
+        
+    }
+});
