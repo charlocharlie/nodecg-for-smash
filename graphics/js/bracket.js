1d2775684d29b9d2e4f2cdcf2de7bca4ebf9ee55
charlocharlie <charlocharlie@yahoo.com>
2016-09-27 16:29:13 -0500

    Updated bracket graphic to match Challonge's new styling.

--- graphics/js/bracket.js
+++ graphics/js/bracket.js
@@ -13,14 +13,14 @@ $( function() {
 			}
 			$( "#" + data.bracket[ i ].roundMatch + "p1 > .name" ).text( data.bracket[ i ].p1name );
 			$( "#" + data.bracket[ i ].roundMatch + "p1 > .score" ).text( data.bracket[ i ].score[ 0 ] );
-			$( "#" + data.bracket[ i ].roundMatch + "p1 > .score" ).css( { "background-color": "#808080" } );
+			$( "#" + data.bracket[ i ].roundMatch + "p1 > .score" ).css( { "background-color": "#787A80" } );
 			$( "#" + data.bracket[ i ].roundMatch + "p2 > .name" ).text( data.bracket[ i ].p2name );
 			$( "#" + data.bracket[ i ].roundMatch + "p2 > .score" ).text( data.bracket[ i ].score[ 1 ] );
-			$( "#" + data.bracket[ i ].roundMatch + "p2 > .score" ).css( { "background-color": "#808080" } );
+			$( "#" + data.bracket[ i ].roundMatch + "p2 > .score" ).css( { "background-color": "#787A80" } );
 			if ( data.bracket[ i ].winner != 0 ) {
 				$( "#" + data.bracket[ i ].roundMatch + "p" + loser[ data.bracket[ i ].winner ] + " > .score" ).css( {
-					"color": "#111",
-					"background-color": "#808080",
+					"color": "#23252D",
+					"background-color": "#787A80",
 					"text-decoration": "none"
 				} );
 				$( "#" + data.bracket[ i ].roundMatch + "p" + loser[ data.bracket[ i ].winner ] + " > .name" ).css( {
@@ -29,8 +29,8 @@ $( function() {
 					"text-decoration": "none"
 				} );
 				$( "#" + data.bracket[ i ].roundMatch + "p" + data.bracket[ i ].winner + " > .score" ).css( {
-					"color": "#111",
-					"background-color": "#f68800",
+					"color": "#23252D",
+					"background-color": "#ff7324",
 					"text-decoration": "none"
 				} );
 				$( "#" + data.bracket[ i ].roundMatch + "p" + data.bracket[ i ].winner + " > .name" ).css( {
